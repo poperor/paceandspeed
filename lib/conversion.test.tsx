d3a368093e34596578547a2fa82dec73e9371161
Per Halvor Tryggeseth <per.halvor@gmail.com>
2021-10-19 19:53:42 +0200

    Even more conversions

--- lib/conversion.test.tsx
+++ lib/conversion.test.tsx
@@ -1,4 +1,4 @@
-import {toKph, fromMinutesPerKm, fromMinutesPerDistanceInMeters} from './conversion'
+import { toKph, fromMinutesPerKm, fromMinutesPerDistanceInMeters, fromMph, fromMinutesPerMile, fromMinutesPerDistanceInYards, toMinutesPerKm } from './conversion'
 
 test('rounds kph correctly', () => {
     expect(toKph(13.46576)).toBe(13.5)
@@ -17,30 +17,49 @@ test('rounds kph without integer without .0 at end', () => {
 })
 
 test('6 min per km to be 10 kph', () => {
-    expect(fromMinutesPerKm({min:6, sec: 0})).toBe(10)
+    expect(fromMinutesPerKm({ min: 6, sec: 0 })).toBe(10)
 })
 
 test('4 min 30 per km to be 13.3333... kph', () => {
-    expect(fromMinutesPerKm({min:4, sec: 30})).toBe(13.333333333333334)
+    expect(fromMinutesPerKm({ min: 4, sec: 30 })).toBe(13.333333333333334)
 })
 
 test('0 min 0 per km to be 0 kph', () => {
-    expect(fromMinutesPerKm({min:0, sec: 0})).toBe(0)
+    expect(fromMinutesPerKm({ min: 0, sec: 0 })).toBe(0)
 })
 
 test('3 min per 500 m to be 10 kph', () => {
-    expect(fromMinutesPerDistanceInMeters({min:3, sec: 0, distance: 500})).toBe(10)
+    expect(fromMinutesPerDistanceInMeters({ min: 3, sec: 0, distance: 500 })).toBe(10)
 })
 
 test('4 min 30 per 0 m to be 0 kph', () => {
-    expect(fromMinutesPerDistanceInMeters({min:4, sec: 30, distance: 0})).toBe(0)
+    expect(fromMinutesPerDistanceInMeters({ min: 4, sec: 30, distance: 0 })).toBe(0)
 })
 
 test('0 min 0 per km to be 0 kph', () => {
-    expect(fromMinutesPerDistanceInMeters({min:0, sec: 0, distance: 500})).toBe(0)
+    expect(fromMinutesPerDistanceInMeters({ min: 0, sec: 0, distance: 500 })).toBe(0)
 })
 
 test('2 min 30 per km to be 13.1 kph (Bislett !)', () => {
-    expect(+fromMinutesPerDistanceInMeters({min:2, sec: 30, distance: 546}).toFixed(1)).toBe(13.1)
+    expect(+fromMinutesPerDistanceInMeters({ min: 2, sec: 30, distance: 546 }).toFixed(1)).toBe(13.1)
 })
 
+test('6.9 mph to be 11.10 rounded', () => {
+    expect(+fromMph(6.9).toFixed(2)).toBe(11.10)
+})
+
+test('7 min per miles to be 13.79 kph rounded', () => {
+    expect(+fromMinutesPerMile({ min: 7, sec: 0 }).toFixed(2)).toBe(13.79)
+})
+
+test('0 min 0 per miles to be 0 kph', () => {
+    expect(fromMinutesPerMile({ min: 0, sec: 0 })).toBe(0)
+})
+
+test('7 min per 1760 yards to be 13.79 rounded', () => {
+    expect(+fromMinutesPerDistanceInYards({ min: 7, sec: 0, distance: 1760 }).toFixed(2)).toBe(13.79)
+})
+
+test('13.333333333333334 kph to 4 min 30 sec per km', () => {
+    expect(toMinutesPerKm(13.333333333333334)).toStrictEqual({min: 4, sec: 30})
+})
--- lib/conversion.ts
+++ lib/conversion.ts
@@ -10,7 +10,7 @@ export interface MinutesPerDistanceInMeters {
     distance: number
 }
 
-export interface MinutesPerMiles {
+export interface MinutesPerMile {
     min: number
     sec: number
 }
@@ -20,8 +20,6 @@ export interface MinutesPerDistanceInYards {
     sec: number
     distance: number
 }
-
-export const toKph = (cannonicalKph: number): number => +cannonicalKph.toFixed(1)
 
 export const fromMinutesPerKm = (minPerKm: MinutesPerKm): number => {
     const sec = minPerKm.min * 60 + minPerKm.sec
@@ -39,4 +37,34 @@ export const fromMinutesPerDistanceInMeters = (minutesPerMeters: MinutesPerDista
     }
     const distancePerKm = minutesPerMeters.distance / 1000
     return (3600 / sec) * distancePerKm
+}
+
+export const fromMph = (mph: number): number => mph * 1.60934
+
+export const fromMinutesPerMile = (minPerMile: MinutesPerMile): number => {
+    const sec = minPerMile.min * 60 + minPerMile.sec
+    if (!sec) {
+        return 0
+    } else {
+        return (3600 / sec) * 1.60934
+    }
+}
+
+export const fromMinutesPerDistanceInYards = (minutesPerYards: MinutesPerDistanceInYards): number => {
+    const sec = minutesPerYards.min * 60 + minutesPerYards.sec
+    if (!minutesPerYards.distance || !sec) {
+        return 0;
+    }
+    const distancePerMile = minutesPerYards.distance / 1760
+    return (3600 / sec) * distancePerMile * 1.60934
+}
+
+export const toKph = (cannonicalKph: number): number => +cannonicalKph.toFixed(1)
+
+export const toMinutesPerKm = (kph: number): MinutesPerKm => {
+    const secPerKm = 3600 / kph
+    return {
+        min: Math.floor(secPerKm / 60),
+        sec: +(secPerKm % 60).toFixed(0),
+    }
 }
