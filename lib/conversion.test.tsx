ec0d91639ddfb0ac109047397f2c45910c7b76e8
Per Halvor Tryggeseth <per.halvor@gmail.com>
2021-10-20 20:32:41 +0200

    finished conversions

--- lib/conversion.test.tsx
+++ lib/conversion.test.tsx
@@ -1,4 +1,16 @@
-import { toKph, fromMinutesPerKm, fromMinutesPerDistanceInMeters, fromMph, fromMinutesPerMile, fromMinutesPerDistanceInYards, toMinutesPerKm } from './conversion'
+import {
+    fromMinutesPerKm,
+    fromMinutesPerDistanceInMeters,
+    fromMph, 
+    fromMinutesPerMile,
+    fromMinutesPerDistanceInYards,
+    toKph,
+    toMinutesPerKm,
+    toMinutesPerDistanceInMeters,
+    toMph,
+    toMinutesPerMile,
+    toMinutesPerDistanceInYards
+} from './conversion'
 
 test('rounds kph correctly', () => {
     expect(toKph(13.46576)).toBe(13.5)
@@ -61,5 +73,21 @@ test('7 min per 1760 yards to be 13.79 rounded', () => {
 })
 
 test('13.333333333333334 kph to 4 min 30 sec per km', () => {
-    expect(toMinutesPerKm(13.333333333333334)).toStrictEqual({min: 4, sec: 30})
+    expect(toMinutesPerKm(13.333333333333334)).toStrictEqual({ min: 4, sec: 30 })
+})
+
+test('13.333333333333334 kph to 2 min 27 sec per 546 m (Bislett!)', () => {
+    expect(toMinutesPerDistanceInMeters(13.333333333333334, 546)).toStrictEqual({ min: 2, sec: 27, distance: 546 })
+})
+
+test('11.10 kph to be 6.9 mph', () => {
+    expect(+toMph(11.1).toFixed(2)).toBe(6.9)
+})
+
+test('13.79 kph to 7 min per mile', () => {
+    expect(toMinutesPerMile(13.79)).toStrictEqual({ min: 7, sec: 0 })
+})
+
+test('13.79 kph to 3 min 30 sec per 880 yards', () => {
+    expect(toMinutesPerDistanceInYards(13.79, 880)).toStrictEqual({ min: 3, sec: 30, distance: 880 })
 })
--- lib/conversion.ts
+++ lib/conversion.ts
@@ -21,6 +21,9 @@ export interface MinutesPerDistanceInYards {
     distance: number
 }
 
+const KphMphRatio = 1.60934
+const YardsPerMile = 1760
+
 export const fromMinutesPerKm = (minPerKm: MinutesPerKm): number => {
     const sec = minPerKm.min * 60 + minPerKm.sec
     if (!sec) {
@@ -39,14 +42,14 @@ export const fromMinutesPerDistanceInMeters = (minutesPerMeters: MinutesPerDista
     return (3600 / sec) * distancePerKm
 }
 
-export const fromMph = (mph: number): number => mph * 1.60934
+export const fromMph = (mph: number): number => mph * KphMphRatio
 
 export const fromMinutesPerMile = (minPerMile: MinutesPerMile): number => {
     const sec = minPerMile.min * 60 + minPerMile.sec
     if (!sec) {
         return 0
     } else {
-        return (3600 / sec) * 1.60934
+        return (3600 / sec) * KphMphRatio
     }
 }
 
@@ -55,8 +58,8 @@ export const fromMinutesPerDistanceInYards = (minutesPerYards: MinutesPerDistanc
     if (!minutesPerYards.distance || !sec) {
         return 0;
     }
-    const distancePerMile = minutesPerYards.distance / 1760
-    return (3600 / sec) * distancePerMile * 1.60934
+    const distancePerMile = minutesPerYards.distance / YardsPerMile
+    return (3600 / sec) * distancePerMile * KphMphRatio
 }
 
 export const toKph = (cannonicalKph: number): number => +cannonicalKph.toFixed(1)
@@ -66,5 +69,34 @@ export const toMinutesPerKm = (kph: number): MinutesPerKm => {
     return {
         min: Math.floor(secPerKm / 60),
         sec: +(secPerKm % 60).toFixed(0),
+    }
+}
+
+export const toMinutesPerDistanceInMeters = (kph: number, distance: number): MinutesPerDistanceInMeters => {
+    const secPerDistance = (3600 / kph) * (distance / 1000) 
+    return {
+        min: Math.floor(secPerDistance / 60),
+        sec: +(secPerDistance % 60).toFixed(0),
+        distance
+    }
+}
+
+export const toMph = (cannonicalKph: number): number => cannonicalKph / KphMphRatio
+
+export const toMinutesPerMile = (kph: number): MinutesPerMile => {
+    const secPerMile = (3600 / kph) * KphMphRatio
+    return {
+        min: Math.floor(secPerMile / 60),
+        sec: +(secPerMile % 60).toFixed(0),
+    }
+}
+
+export const toMinutesPerDistanceInYards = (kph: number, distance: number): MinutesPerDistanceInYards => {
+    const mph = kph / KphMphRatio
+    const secPerDistance = (2.04545455 / mph) * distance
+    return {
+        min: Math.floor(secPerDistance / 60),
+        sec: +(secPerDistance % 60).toFixed(0),
+        distance
     }
 }
