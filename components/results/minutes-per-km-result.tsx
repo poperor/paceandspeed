0697a098b3351a283421d33f2bfc57145fb23ec2
Per Halvor Tryggeseth <per.halvor@gmail.com>
2023-02-23 16:05:59 +0100

    outfactored leading zeros function to lib

--- components/results/minutes-per-km-result.tsx
+++ components/results/minutes-per-km-result.tsx
@@ -1,8 +1,7 @@
 import { ResultProps, toMinutesPerKm } from "../../lib/conversion";
 import resultStyles from "./result.module.css";
 import utilityStyles from '../../styles/utility.module.css'
-
-
+import withLeadZeroes from "../../lib/with-lead-zeroes";
 
 export const MinutesPerKmResult = ({ cannonicalKph }: ResultProps) => {
   const minutesPerKm = toMinutesPerKm(cannonicalKph);
@@ -10,8 +9,8 @@ export const MinutesPerKmResult = ({ cannonicalKph }: ResultProps) => {
     <div>
       {minutesPerKm && (
         <div className={`${utilityStyles.verticalAlign} ${resultStyles.result}`}>
-          {minutesPerKm.min > 0 && <span>{minutesPerKm.min}</span>}:
-          {minutesPerKm.sec > 0 && <span>{minutesPerKm.sec >= 10 ? minutesPerKm.sec : "0" + minutesPerKm.sec} </span>}
+          <span>{withLeadZeroes(minutesPerKm.min)}</span>:
+          <span>{withLeadZeroes(minutesPerKm.sec)} </span>
         </div>
       )}
     </div>
